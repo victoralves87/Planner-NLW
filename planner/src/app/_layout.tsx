bdbaee281fe04dfafd7b531a74fd36fd84a4da3a
victor alves <voctorralves@gmail.com>
2024-07-09 20:03:13 -0300

    subindo estrutura base do projeto

---
+++ planner/src/app/_layout.tsx
@@ -0,0 +1,35 @@
+import  "@/styles/global.css"
+import { View, StatusBar } from "react-native" 
+import { Slot } from "expo-router"
+import { Loading } from "@/components/loading"
+import {
+    useFonts,
+    Inter_500Medium,
+    Inter_400Regular,
+    Inter_600SemiBold,
+
+} from "@expo-google-fonts/inter"
+
+
+export default function Layout(){
+    const[fontsLoaded] = useFonts({
+        Inter_500Medium,
+        Inter_400Regular,
+        Inter_600SemiBold,
+    })
+
+    if(!fontsLoaded){
+        return <Loading/>
+    }
+
+    return(
+        <View className="flex-1 bg-zinc-950">
+            <StatusBar 
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+            />
+            <Slot/>
+        </View>
+    )
+}
---
+++ planner/src/app/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text, Image} from "react-native"
+
+export default function Index(){
+    return(
+        <View className="flex-1 items-center justify-center">
+            <Image source={require("@/assets/logo.png")} className="h-8" resizeMode="contain"/>
+            <Text className="text-zinc-400 font-regular text-center text-lg m-3">
+                Convide seus amigos e planeje sua {"\n"} próxima viagem!</Text>
+        </View>
+    )
+}
